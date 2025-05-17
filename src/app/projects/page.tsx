3ec17c502fd8be2754b059cc7aa102fbcffc07ed
Abijith Suresh <abijithsuresh47@gmail.com>
2025-05-17 06:35:38 +0000

    chore: update projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,11 +5,17 @@ export default async function ProjectsPage() {
   const projects = await getProjects()
 
   return (
-    <section className='pb-24 pt-40'>
+    <section className='pt-40 pb-24'>
       <div className='container max-w-3xl'>
         <h1 className='title mb-12'>Projects</h1>
+          <div className='text-center'>
+            <p className='font-serif text-muted-foreground text-xl'>
+              Our project showcase is currently under construction. Stay
+              tuned for exciting updates!
+            </p>
+          </div>
 
-        <Projects projects={projects} />
+        {/* <Projects projects={projects} /> */}
       </div>
     </section>
   )
