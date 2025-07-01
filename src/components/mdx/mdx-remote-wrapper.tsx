577a31355b62fb579fb44af7fe7b32f43e0c826a
Abijith Suresh <abijithsuresh47@gmail.com>
2025-07-01 10:27:58 +0000

    Fix: Resolve MDX suspense error in client components

--- src/components/mdx/mdx-remote-wrapper.tsx
+++ src/components/mdx/mdx-remote-wrapper.tsx
@@ -1,32 +1,16 @@
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
-import { highlight } from 'sugar-high'
-import { useMDXComponents } from '../../../mdx-components'
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function Code({ children, ...props }: any) {
-  const codeHTML = highlight(children)
-  return (
-    <code
-      dangerouslySetInnerHTML={{ __html: codeHTML }}
-      className='bg-muted rounded px-[0.3rem] py-[0.2rem] font-mono text-sm'
-      {...props}
-    />
-  )
-}
+import { mdxServerComponents } from '@/lib/mdx-server-components'
 
 interface MDXRemoteWrapperProps extends MDXRemoteProps {
   children?: React.ReactNode
 }
 
 export async function MDXRemoteWrapper(props: MDXRemoteWrapperProps) {
-  const mdxComponents = useMDXComponents({})
-
   return (
     <MDXRemote
       {...props}
       components={{
-        ...mdxComponents,
-        code: Code,
+        ...mdxServerComponents,
         ...(props.components || {})
       }}
     />
