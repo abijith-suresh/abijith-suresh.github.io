9bd53befba89e1745263c425bbbc92b2e740854d
Abijith Suresh <58354654+abijith-suresh@users.noreply.github.com>
2025-07-13 19:53:23 +0000

    fix: resolve prettier formatting issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
 
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
