907418f57faa382ddf28c8d02bfd37fdd6801b98
Abijith Suresh <58354654+abijith-suresh@users.noreply.github.com>
2026-01-04 10:42:35 +0000

    chore: add tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
