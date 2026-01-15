d890f11ef41d4ac757da438ce4b9a0f36032af8f
Abijith Suresh <58354654+abijith-suresh@users.noreply.github.com>
2026-01-15 18:26:18 +0000

    feat: add Image, Video, LinkCard MDX components

--- src/components/mdx/index.ts
+++ src/components/mdx/index.ts
@@ -1,1 +1,5 @@
 export { default as Callout } from "./Callout.astro";
+export { default as Image } from "./Image.astro";
+export { default as LinkCard } from "./LinkCard.astro";
+export { default as Video } from "./Video.astro";
+
